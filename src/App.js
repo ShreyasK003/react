4033aea5090fa6d1c7708f00f4eea6d8e39f0e49
ShreyasK003 <89639350+ShreyasK003@users.noreply.github.com>
2023-05-09 20:40:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,40 @@ import './App.css';
 
 function App() {
 
+  const [Users, setUsers] = useState([])
+
   (async()=>{
     const url = await fetch("https://sunglasses-dugong.cyclic.app/")
     const data = await url.json()
+    setUsers(data)
     console.log(data)
   })()
 
   return (
     <div className="App">
-      hello world
+      <table className={`table table-striped table-${Mode} table-hover`}>
+        <thead>
+          <tr>
+            <th scope="col">Username</th>
+            <th scope="col">Email</th>
+            <th scope="col">Password</th>
+            <th scope="col">Reg. Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Users.map(user => (
+            <tr key={user.id}>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.password}</td>
+              <td>{user.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
+
 
 export default App;
